622cd4a85aab3a12bc61d85d7f75f10bb6123f80
Corentin MARQUET <cormarquet@gmail.com>
2024-12-27 20:24:34 +0100

    Improves toast when conflict during booking

--- app/src/main/java/com/roomreservation/service/BookingsApiService.kt
+++ app/src/main/java/com/roomreservation/service/BookingsApiService.kt
@@ -2,7 +2,12 @@ package com.roomreservation.service
 
 import com.roomreservation.model.BookingDto
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface BookingsApiService {
     @POST("bookings")
